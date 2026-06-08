8af7619f72f808251d8ab70ee386a2d3f43fb211
NicolasJesusz <nicolassantosdejesus22@gmail.com>
2026-06-08 01:58:25 -0300

    tabela agendamentos em sql server

---
+++ agendamentos.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Agendamentos] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [NomeCliente]     NVARCHAR (100) NOT NULL,
+    [Telefone]        NVARCHAR (20)  NOT NULL,
+    [DataAgendamento] DATE           NOT NULL,
+    [HoraAgendamento] NVARCHAR (10)  NOT NULL,
+    [Servico]         NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
